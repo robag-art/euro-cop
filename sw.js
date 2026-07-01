7c0638dbfced855b1b601d926722d55136dc7efd
robag-art <rob.ag@hotmail.fr>
2026-07-01 18:57:48 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
 const CACHE = 'eur-cop-v1';
-const ASSETS = ['/', '/index.html'];
+const ASSETS = ['/euro-cop/', '/euro-cop/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
